a0c0ac802c95d36c6392b71a5ba91e06a1f13870
MrException <robmcbride@gmail.com>
2010-06-03 02:05:05 -0700

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject colours "1.0.0-SNAPSHOT"
+            :description "A colour based game."
+            :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                           [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+            :dev-dependencies [[vimclojure "2.2.0-SNAPSHOT"]]
+            :main colours.colours)
---
+++ test/colours/core_test.clj
@@ -0,0 +1,6 @@
+(ns colors.core-test
+  (:use [colors.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false))
