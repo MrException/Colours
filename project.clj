a488b32b70bc732116667cb29e5f99bf1a813abb
Robert McBride <robmcbride@gmail.com>
2011-11-02 11:27:20 -0700

    restarting it all! moving to JavaScript and canvas, away from Clojure :)

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject colours "1.0.0-SNAPSHOT"
-            :description "A colour based game."
-            :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                           [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
-            :dev-dependencies [[jline "0.9.94"]]
-            :main colours.colours)
--- test/colours/core_test.clj
+++
@@ -1,6 +0,0 @@
-(ns colors.core-test
-  (:use [colors.core] :reload-all)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false))
